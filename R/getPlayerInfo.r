f27c1a2577168b6364b25e140d1f9b144adeb4ba
zfunk <nicholas.zani@googlemail.com>
2015-06-08 16:34:26 +0100

    Added country to search

--- R/getPlayerInfo.r
+++ R/getPlayerInfo.r
@@ -1,7 +1,8 @@
 #' getPlayerInfo
 #'
 #' Scrapes player info from cricinfo
-#' @param view Do you want to show in a table? Defaults to TRUE.
+#' @param PlayerSearch 		Name of the player to search Cricinfo for
+#' @param PlayerCountry 	Country the player plays for
 #' @param PlayerNumber		Cricinfo player number
 #' @param BattingBowling	Batting or bowling figures. Default is batting
 #' @param ViewType			Data to be returned. Default is average
@@ -11,110 +12,113 @@
 #' playerInfo <- getPlayerInfo(PlayerNumber="11728", BattingBowling = "Batting", 
 #'					ViewType = "Inn", Clean="Y")
 
-getPlayerInfo <- function(PlayerSearch, PlayerNumber="", BattingBowling="Batting", ViewType="Ave", Clean="Y"){
+getPlayerInfo <- function(PlayerSearch, PlayerNumber="", PlayerCountry = "England", BattingBowling="Batting", ViewType="Ave", Clean="Y"){
+
+	if (!(PlayerCountry %in% c("England","Australia","New Zealand","Bangladesh","Pakistan","India","Sri Lanka","West Indies","South Africa","Zimbabwe")))
+		{ return(cat("ERROR: Country not in list. Must be one of England, Australia, New Zealand, Bangladesh \n Pakistan, India, Sri Lanka, West Indies, South Africa, Zimbabwe"))}
   
-  if (PlayerNumber != ""){
+	if (PlayerNumber != ""){
   
-	theurl <- paste0("http://stats.espncricinfo.com/ci/engine/player/",PlayerNumber,".html?class=1;template=results;type=",tolower(BattingBowling),";view=innings")
-	tables <- readHTMLTable(theurl)
+		theurl <- paste0("http://stats.espncricinfo.com/ci/engine/player/",PlayerNumber,".html?class=1;template=results;type=",tolower(BattingBowling),";view=innings")
+		tables <- readHTMLTable(theurl)
 	  
 	}
 	
 	else {
 	
-	number_play <- getPlayerNumber(PlayerSearch)
+		number_play <- getPlayerNumber(PlayerSearch, CountrySearch = PlayerCountry)
 	
-	if (length(number_play) == 0) { return(cat("ERROR: Player name spelt incorrectly"))}
-	
-	theurl <- paste0("http://stats.espncricinfo.com/ci/engine/player/",number_play,".html?class=1;template=results;type=",tolower(BattingBowling),";view=innings")
-	tables <- readHTMLTable(theurl)
+		if (length(number_play) == 0) { return(cat("ERROR: Player name spelt incorrectly"))}
+		
+		theurl <- paste0("http://stats.espncricinfo.com/ci/engine/player/",number_play,".html?class=1;template=results;type=",tolower(BattingBowling),";view=innings")
+		tables <- readHTMLTable(theurl)
 	
 	}
   
-  if (ViewType=="Ave"){
-    
-    keptTable <- as.data.frame(tables[3], stringsAsFactors = FALSE)
-      
-  }
+	  if (ViewType=="Ave"){
+		
+		keptTable <- as.data.frame(tables[3], stringsAsFactors = FALSE)
+		  
+	  }
   
-  else {
-    
-    keptTable <- as.data.frame(tables[4], stringsAsFactors = FALSE)
-    
-  }
-  
-  if (Clean != "Y"){
-    
-    return(as.data.frame(keptTable, stringsAsFactors = FALSE))
-    
-  }
-  
-  else if (ViewType !="Ave" & BattingBowling=="Batting"){
-    
-    names(keptTable) <- c("Runs","Mins","BallsFaced","No4s","No6s","SR","Pos","Dismissal","Inns","Blank","Opposition","Ground","StartDate","Test")
-    
-    keptTable$Blank <- NULL
-    keptTable$Test <- NULL
-    
-    keptTable$NOFlag <- ifelse(grepl("\\*",keptTable$Runs),1,0)
-    
-    keptTable$Runs <- str_replace_all(keptTable$Runs, "\\*","")
-    
-    keptTable$Runs <- as.numeric(as.character(keptTable$Runs))
-    keptTable$Mins <- as.numeric(as.character(keptTable$Mins))
-    keptTable$BallsFaced <- as.numeric(as.character(keptTable$BallsFaced))
-    keptTable$No4s <- as.numeric(as.character(keptTable$No4s))
-    keptTable$No6s <- as.numeric(as.character(keptTable$No6s))
-    keptTable$SR <- as.numeric(as.character(keptTable$SR))
-    keptTable$Pos <- as.numeric(as.character(keptTable$Pos))
-    
-    keptTable$Opposition <- str_replace_all(keptTable$Opposition, "v ","")
-    
-    keptTable <- subset(keptTable, !is.na(keptTable$Runs))
-    
-    ## add an innings number here
-    
-    keptTable$InningsNo <- 1:nrow(keptTable)
-    
-  }
-  
-  else if (ViewType !="Ave" & BattingBowling=="Bowling"){
-    
-    names(keptTable)
-    
-    names(keptTable) <- c("Overs","Maidens","Runs","Wickets","Econ","Pos","Inns","Blank","Opposition","Ground","StartDate","Test")
-    
-    keptTable$Blank <- NULL
-    keptTable$Test <- NULL
-    
-	keptTable$Overs <- as.numeric(as.character(keptTable$Overs))
-	keptTable$Maidens <- as.numeric(as.character(keptTable$Maidens))
-    keptTable$Runs <- as.numeric(as.character(keptTable$Runs))
-    keptTable$Wickets <- as.numeric(as.character(keptTable$Wickets))
-    keptTable$Econ <- as.numeric(as.character(keptTable$Econ))
-    keptTable$Inns <- as.numeric(as.character(keptTable$Inns))
-    keptTable$Pos <- as.numeric(as.character(keptTable$Pos))
-    
-    keptTable$Opposition <- str_replace_all(keptTable$Opposition, "v ","")
-    
-    keptTable <- subset(keptTable, !is.na(keptTable$Overs))
-    
-    ## add an innings number here
-    
-    keptTable$InningsNo <- 1:nrow(keptTable)
-    
-  }
-  
-  else if (ViewType =="Ave" & BattingBowling=="Batting"){
-    
-    names(keptTable) <- c("Overall","CareerSpan","Matches","Innings","NOs","Runs","HS","Ave","BF","SR","100s","50s","0s","4s","6s","Profile")
-    
-    keptTable$Overall <- NULL
-    keptTable$Profile <- NULL
-  
-  }
-  
-  keptTable
+	  else {
+		
+		keptTable <- as.data.frame(tables[4], stringsAsFactors = FALSE)
+		
+	  }
+	  
+	  if (Clean != "Y"){
+		
+		return(as.data.frame(keptTable, stringsAsFactors = FALSE))
+		
+	  }
+	  
+	  else if (ViewType !="Ave" & BattingBowling=="Batting"){
+		
+		names(keptTable) <- c("Runs","Mins","BallsFaced","No4s","No6s","SR","Pos","Dismissal","Inns","Blank","Opposition","Ground","StartDate","Test")
+		
+		keptTable$Blank <- NULL
+		keptTable$Test <- NULL
+		
+		keptTable$NOFlag <- ifelse(grepl("\\*",keptTable$Runs),1,0)
+		
+		keptTable$Runs <- str_replace_all(keptTable$Runs, "\\*","")
+		
+		keptTable$Runs <- as.numeric(as.character(keptTable$Runs))
+		keptTable$Mins <- as.numeric(as.character(keptTable$Mins))
+		keptTable$BallsFaced <- as.numeric(as.character(keptTable$BallsFaced))
+		keptTable$No4s <- as.numeric(as.character(keptTable$No4s))
+		keptTable$No6s <- as.numeric(as.character(keptTable$No6s))
+		keptTable$SR <- as.numeric(as.character(keptTable$SR))
+		keptTable$Pos <- as.numeric(as.character(keptTable$Pos))
+		
+		keptTable$Opposition <- str_replace_all(keptTable$Opposition, "v ","")
+		
+		keptTable <- subset(keptTable, !is.na(keptTable$Runs))
+		
+		## add an innings number here
+		
+		keptTable$InningsNo <- 1:nrow(keptTable)
+		
+	  }
+	  
+	  else if (ViewType !="Ave" & BattingBowling=="Bowling"){
+		
+		names(keptTable)
+		
+		names(keptTable) <- c("Overs","Maidens","Runs","Wickets","Econ","Pos","Inns","Blank","Opposition","Ground","StartDate","Test")
+		
+		keptTable$Blank <- NULL
+		keptTable$Test <- NULL
+		
+		keptTable$Overs <- as.numeric(as.character(keptTable$Overs))
+		keptTable$Maidens <- as.numeric(as.character(keptTable$Maidens))
+		keptTable$Runs <- as.numeric(as.character(keptTable$Runs))
+		keptTable$Wickets <- as.numeric(as.character(keptTable$Wickets))
+		keptTable$Econ <- as.numeric(as.character(keptTable$Econ))
+		keptTable$Inns <- as.numeric(as.character(keptTable$Inns))
+		keptTable$Pos <- as.numeric(as.character(keptTable$Pos))
+		
+		keptTable$Opposition <- str_replace_all(keptTable$Opposition, "v ","")
+		
+		keptTable <- subset(keptTable, !is.na(keptTable$Overs))
+		
+		## add an innings number here
+		
+		keptTable$InningsNo <- 1:nrow(keptTable)
+		
+	  }
+	  
+	  else if (ViewType =="Ave" & BattingBowling=="Batting"){
+		
+		names(keptTable) <- c("Overall","CareerSpan","Matches","Innings","NOs","Runs","HS","Ave","BF","SR","100s","50s","0s","4s","6s","Profile")
+		
+		keptTable$Overall <- NULL
+		keptTable$Profile <- NULL
+	  
+	  }
+	  
+	  keptTable
   
 }
 
--- R/getPlayerNumber.r
+++ R/getPlayerNumber.r
@@ -7,17 +7,40 @@
 #' getPlayerNumber(Name="Stuart Broad")
 
 
-getPlayerNumber <- function(Name){
-  
-  # get the final initial to search for
-  finalword <- toupper(tail(strsplit(Name,split=" ")[[1]],1))
-  initial_Use <- head(strsplit(finalword,"")[[1]],1)
-  url_search <- paste0("http://www.espncricinfo.com/ci/content/player/country.html?country=1;alpha=",initial_Use)
-  table_search <- as.data.frame(toupper(as.character(readLines(url_search))), stringsAsFactors = FALSE)
-  names(table_search) <- c("Col1")
-  name_row  <-   which(grepl(toupper(Name),table_search$Col1))
-  player_url <- table_search[name_row-1,1]
-  player_url_clean <- as.numeric(gsub(".HTML\"CLASS=\"COLUMNISTSMRY\"STYLE=\"VERTICAL-ALIGN:MIDDLE;\">","",gsub("<AHREF=\"/CI/CONTENT/PLAYER/","",gsub(" ","",player_url))))
-  player_url_clean
+getPlayerNumber <- function(Name, CountrySearch){
+
+	# get the country number
+	
+	if (CountrySearch == "England")
+		{CountryNumber <- 1}
+	else if (CountrySearch == "Australia")	
+		{CountryNumber <- 2}
+	else if (CountrySearch == "Bangladesh")	
+		{CountryNumber <- 25}
+	else if (CountrySearch == "India")	
+		{CountryNumber <- 6}	
+	else if (CountrySearch == "New Zealand")	
+		{CountryNumber <- 5}	
+	else if (CountrySearch == "Pakistan")	
+		{CountryNumber <- 7}	
+	else if (CountrySearch == "South Africa")	
+		{CountryNumber <- 3}		
+	else if (CountrySearch == "Sri Lanka")	
+		{CountryNumber <- 8}
+	else if (CountrySearch == "West Indies")	
+		{CountryNumber <- 4}		
+	else if (CountrySearch == "Zimbabwe")	
+		{CountryNumber <- 9}			
+ 
+	# get the final initial to search for
+	finalword <- toupper(tail(strsplit(Name,split=" ")[[1]],1))
+	initial_Use <- head(strsplit(finalword,"")[[1]],1)
+	url_search <- paste0("http://www.espncricinfo.com/ci/content/player/country.html?country=",CountryNumber,";alpha=",initial_Use)
+	table_search <- as.data.frame(toupper(as.character(readLines(url_search))), stringsAsFactors = FALSE)
+	names(table_search) <- c("Col1")
+	name_row  <-   which(grepl(toupper(Name),table_search$Col1))
+	player_url <- table_search[name_row-1,1]
+	player_url_clean <- as.numeric(gsub(".HTML\"CLASS=\"COLUMNISTSMRY\"STYLE=\"VERTICAL-ALIGN:MIDDLE;\">","",gsub("<AHREF=\"/CI/CONTENT/PLAYER/","",gsub(" ","",player_url))))
+	player_url_clean
   
 }   
