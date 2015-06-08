ba7eaf3d4042e857aa7c4bcce8537f24f7873f79
zfunk <nicholas.zani@googlemail.com>
2015-06-08 16:34:40 +0100

    Aggregates country averages

---
+++ R/countryAggregates.r
@@ -0,0 +1,19 @@
+#' countryAggregates
+#'
+#' Creates a dataframe giving player averages against countries played against
+#' @param PlayerName			Player name to be searched for in the cricinfo database
+#' @export
+#' @examples
+#' Aggregates <- countryAggregates(PlayerName="Alastair Cook")
+
+
+countryAggregates <- function(PlayerName, PlayerCountry){
+
+	rawdata <- getPlayerInfo(PlayerSearch = PlayerName, PlayerCountry = PlayerCountry, PlayerNumber="", BattingBowling="Batting", ViewType="Inns", Clean="Y")
+	
+	aggregates <- sqldf("select Opposition, count(*) as Total_Inns, sum(NOFlag) as NOs, sum(Runs) as Total_Runs,  sum(Runs) / ( count(*) - sum(NOFlag)) as Ave from rawdata where Opposition != 'ICC World XI' group by Opposition")
+	
+	aggregates[ order(-aggregates[,"Ave"]), ]
+	
+}  
+		
